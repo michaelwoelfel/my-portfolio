f86a6d867aa1a608bcf7e35f35997007dc8765e3
Michel <woelfelm@t-online.de>
2023-10-28 12:51:51 +0200

    added lets work component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LetsWorkComponent } from './lets-work/lets-work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponentComponent
+    HeaderComponentComponent,
+    LetsWorkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
