52b87779d49cebeb90dc52936ed673e0e8ab47b5
Michel <woelfelm@t-online.de>
2023-11-01 19:51:51 +0100

    completet Contact and added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
@@ -9,6 +8,18 @@ import { LetsWorkComponent } from './lets-work/lets-work.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { MyWorkComponent } from './my-work/my-work.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {FormControl, Validators, FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSelectModule} from '@angular/material/select';
+import { FooterComponent } from './footer/footer.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -17,12 +28,21 @@ import { ContactComponent } from './contact/contact.component';
     LetsWorkComponent,
     SkillSetComponent,
     MyWorkComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent,
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule ,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Component, VERSION, } from "@angular/core";
+import { FormBuilder, FormGroup, FormControl, Validators, } from "@angular/forms";
+
+interface EmailData {
+  name: string | null;
+  email: string | null;
+  message: string | null;
+}
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+
 })
 export class ContactComponent {
 
+  title = "Formspree.io Angular " + VERSION.major + " Example";
+  secretKey: string = "xeqbdjnq";
+  emailForm: FormGroup;
+ 
+
+  constructor(private fb: FormBuilder, private httpClient: HttpClient) {
+    this.emailForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      name: new FormControl('', [Validators.required, Validators.minLength(5),]),
+      message: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      disableSelect : new FormControl(false),
+    });
+  }
+
+
+  onSubmitForm() {
+    let emailData: EmailData = this.emailForm.value as EmailData;
+    this.sendEmail(emailData);
+  }
+
+  getErrorMessage() {
+    if (this.emailForm.get('email')?.hasError('required')) {
+      return 'Please fill in a valid e-mail';
+    }
+    return this.emailForm.get('email')?.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  get email() {
+    return this.emailForm.get('email') as FormControl;
+  }
+
+  get disableSelect() {
+    return this.emailForm.get('disableSelect') as FormControl;
+  }
+
+
+  get name() {
+    return this.emailForm.get('name') as FormControl;
+  }
+
+  get message() {
+    return this.emailForm.get('message') as FormControl;
+  }
+
+
+  sendEmail(value: EmailData) {
+    let url = "https://formspree.io/f/" + this.secretKey;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      })
+    };
+    let data = `name=${value.name}&email=${value.email}&message=${value.message}`;
+    let errorMessage: string = "";
+    this.httpClient.post<any>(url, data, httpOptions).subscribe({
+      next: data => {
+        console.log("email sent" + JSON.stringify(data));
+      },
+      error: error => {
+        errorMessage = error.message;
+        console.log('error!', errorMessage);
+      }
+    })
+    console.log("url is ", url);
+    console.log("data", value);
+  }
+
+
+
 }
+
+
+
+
