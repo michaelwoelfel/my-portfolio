70ef30b3e9e21b57985434cc4f61112a1084ad55
michaelwoelfel <woelfelm@t-online.de>
2023-12-19 09:12:44 +0100

    added dabubble

--- src/app/projects.constant.ts
+++ src/app/projects.constant.ts
@@ -1,4 +1,24 @@
 export const PROJECTS = [
+    
+
+    {
+        "img": "dabubble.png",
+        "title": "Dabubble",
+        "skills": "Angular| Typescript | Material | Firebase",
+        "link": "https://michael-woelfel.com/Mein%20Join/",
+        "description": "Slack like chat programm build with Angular, used Firebase as backend.",
+        "github": "https://github.com/michaelwoelfel/Join-Kanban-Board",
+    },
+
+    {
+        "img": "el-pollo-loco.png",
+        "title": "Pollo Loco",
+        "skills": "HTML | CSS | JavaScript",
+        "link": "https://michael-woelfel.com/El%20Pollo%20Loco/index.html",
+        "description": "Jump, run and throw game based object-oriented approach. Help Pepe to find coins and tabasco to fight against the big endboss.",
+        "github": "https://github.com/michaelwoelfel/el-pollo-loco",
+    },
+
     {
         "img": "join.png",
         "title": "Join",
@@ -6,14 +26,6 @@ export const PROJECTS = [
         "link": "https://michael-woelfel.com/Mein%20Join/",
         "description": "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories",
         "github": "https://github.com/michaelwoelfel/Join-Kanban-Board",
-    },
-    {
-        "img": "el-pollo-loco.png",
-        "title": "Pollo Loco",
-        "skills": "HTML| CSS | JavaScript",
-        "link": "https://michael-woelfel.com/El%20Pollo%20Loco/index.html",
-        "description": "Jump, run and throw game based object-oriented approach. Help Pepe to find coins and tabasco to fight against the big endboss.",
-        "github": "https://github.com/michaelwoelfel/el-pollo-loco",
     },
 
     {
--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -10,58 +10,70 @@ import { ResponsiveService } from '../responsive.service';
 
 
 export class SkillSetComponent {
-constructor(public responsiveService: ResponsiveService) {}
- 
- skills = [
+  constructor(public responsiveService: ResponsiveService) { }
 
-    { 
-      
+  skills = [
+
+    {
+
       "skill-pic": "Angular.png",
       "skill-name": "Angular",
     },
-    // {
-    //   "skill-pic": "Typescript.png",
-    //   "skill-name": "Typescript",
-    // },
     {
-      
+      "skill-pic": "Material.png",
+      "skill-name": "Material",
+    },
+
+    {
+
+      "skill-pic": "Firebase.png",
+      "skill-name": "Firebase",
+    },
+
+    {
+      "skill-pic": "Typescript.png",
+      "skill-name": "Typescript",
+    },
+    {
       "skill-pic": "Javascript.png",
       "skill-name": "JavaScript",
     },
+
     {
-      
       "skill-pic": "HTML.png",
       "skill-name": "HTML",
     },
 
-    // {
-    //   "skill-pic": "Material.png",
-    //   "skill-name": "Material",
-    // },
-    { 
-      
-      "skill-pic": "Firebase.png",
-      "skill-name": "Firebase",
-    },
-    
-   
     {
-      
-      "skill-pic": "git.png",
-      "skill-name": "Git",
-    },
-    {
-      
       "skill-pic": "CSS.png",
       "skill-name": "CSS",
     },
+
+    {
+
+      "skill-pic": "API.png",
+      "skill-name": "APIs",
+    },
+
+    {
+      "skill-pic": "git.png",
+      "skill-name": "Git",
+    },
+
+    {
+      "skill-pic": "scrum.png",
+      "skill-name": "Scrum",
+    },
    
+   
+   
+
   ]
 
-  addShadow(i:number) {
-      let clickedSkill = document.getElementById("skill" + i);
-      clickedSkill?.classList.add("skill-selected");
-      console.log(clickedSkill);
+  addShadow(i: number) {
+    let clickedSkill = document.getElementById("skill" + i);
+    clickedSkill?.classList.add("skill-selected");
+    console.log(clickedSkill);
   }
 
 
