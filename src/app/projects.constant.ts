8de0ffbd228fbca7c9106ec000c6527b3b0060e5
michaelwoelfel <woelfelm@t-online.de>
2024-03-22 15:49:10 +0700

    desginverbesserungen

--- src/app/projects.constant.ts
+++ src/app/projects.constant.ts
@@ -1,33 +1,36 @@
 export const PROJECTS = [
     
-
     {
         "img": "dabubble.png",
         "title": "Dabubble",
-        "skills": "Angular | Typescript | Firebase",
+        "skills": "Angular | TypeScript | Firebase",
         "link": "https://michael-woelfel.com/dabubble",
-        "description": "Slack like chat programm build with Angular, used Firebase as backend.",
-        "github": "https://github.com/michaelwoelfel/",
+        "description": "Developed a Slack-style chat app with Firebase integration for live data sync and storage. Implemented chat rooms and DMs for enhanced communication and teamwork. Utilized Angular and TypeScript for a robust front end, with Firebase back end services and Angular Material for design finesse.",
+        "github": "https://github.com/michaelwoelfel/"
     },
+    
+
+
 
     {
         "img": "el-pollo-loco.png",
-        "title": "Pollo Loco",
+        "title": "El Pollo Loco",
         "skills": "HTML | CSS | JavaScript",
         "link": "https://michael-woelfel.com/El%20Pollo%20Loco/index.html",
-        "description": "Jump, run and throw game based object-oriented approach. Help Pepe to find coins and tabasco to fight against the big endboss.",
-        "github": "https://github.com/michaelwoelfel/el-pollo-loco",
+        "description": "Designed and programmed a 2D sidescroller inspired by classic Super Mario, focusing on object-oriented JavaScript. Crafted engaging levels with HTML/CSS and implemented game mechanics like character movement and enemies, alongside collision detection for responsive gameplay.",
+        "github": "https://github.com/michaelwoelfel/el-pollo-loco"
     },
+    
 
     {
         "img": "join.jpg",
         "title": "Join",
-        "skills": "HTML | CSS | JavaScript ",
+        "skills": "HTML | CSS | JavaScript",
         "link": "https://michael-woelfel.com/Mein%20Join/",
-        "description": "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories",
-        "github": "https://github.com/michaelwoelfel/Join-Kanban-Board",
+        "description": "Developed a Kanban-style task management and team collaboration board with drag-and-drop interfaces for task organization and progress tracking. Built with JavaScript, HTML, and CSS for a clean, user-friendly interface, featuring account integration for a personalized experience.",
+        "github": "https://github.com/michaelwoelfel/Join-Kanban-Board"
     },
-
+    
     // {
     //     "img": "ring-of-fire.png",
     //     "title": "Ring of Fire",
