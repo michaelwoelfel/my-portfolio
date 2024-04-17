286c3f1b0449c6a0f1f38734cacb6300d93244e0
michaelwoelfel <woelfelm@t-online.de>
2024-04-17 12:45:09 +0700

    translation function

--- src/app/projects.constant.ts
+++ src/app/projects.constant.ts
@@ -5,7 +5,7 @@ export const PROJECTS = [
         "title": "Dabubble",
         "skills": "Angular | TypeScript | Firebase",
         "link": "https://michael-woelfel.com/dabubble",
-        "descriptiongerman": "Entwickelte eine Slack-ähnliche Chat-App mit Firebase-Integration für die Synchronisation und Speicherung von Live-Daten. Implementierte Chaträume und Direktnachrichten für verbesserte Kommunikation und Teamarbeit. Nutzte Angular und TypeScript für eine robuste Frontend-Entwicklung, mit Firebase-Backend-Services und Angular Material für gestalterische Feinheit.",
+        "descriptiongerman": "Slack-ähnliche Chat-App mit Firebase-Integration für die Synchronisation und Speicherung von Live-Daten. Implementierte Chaträume und Direktnachrichten für verbesserte Kommunikation und Teamarbeit. Nutzte Angular und TypeScript für eine robuste Frontend-Entwicklung, mit Firebase-Backend-Services und Angular Material für gestalterische Feinheit.",
         "description": "Developed a Slack-style chat app with Firebase integration for live data sync and storage. Implemented chat rooms and DMs for enhanced communication and teamwork. Utilized Angular and TypeScript for a robust front end, with Firebase back end services and Angular Material for design finesse.",
         "github": "https://github.com/michaelwoelfel/"
     },
@@ -18,7 +18,7 @@ export const PROJECTS = [
         "title": "El Pollo Loco",
         "skills": "HTML | CSS | JavaScript",
         "link": "https://michael-woelfel.com/El%20Pollo%20Loco/index.html",
-        "descriptiongerman": "Entwarf und programmierte einen 2D-Seitenscroller, inspiriert von klassischen Super Mario-Spielen, mit einem Fokus auf objektorientiertem JavaScript. Gestaltete fesselnde Level mit HTML/CSS und implementierte Spielmechaniken wie Charakterbewegungen und Gegner, zusammen mit Kollisionserkennung für ein reaktives Spielerlebnis.",
+        "descriptiongerman": "2D-Scroller, inspiriert von klassischen Super Mario-Spielen, mit einem Fokus auf objektorientiertem JavaScript. Gestaltete fesselnde Level mit HTML/CSS und implementierte Spielmechaniken wie Charakterbewegungen und Gegner, zusammen mit Kollisionserkennung für ein reaktives Spielerlebnis.",
 
         "description": "Designed and programmed a 2D sidescroller inspired by classic Super Mario, focusing on object-oriented JavaScript. Crafted engaging levels with HTML/CSS and implemented game mechanics like character movement and enemies, alongside collision detection for responsive gameplay.",
         "github": "https://github.com/michaelwoelfel/el-pollo-loco"
@@ -30,7 +30,7 @@ export const PROJECTS = [
         "title": "Join",
         "skills": "HTML | CSS | JavaScript",
         "link": "https://michael-woelfel.com/Mein%20Join/",
-        "descriptiongerman": "Entwickelte ein Kanban-ähnliches Aufgabenverwaltungs- und Team-Kollaborationsboard mit Drag-and-Drop-Schnittstellen zur Organisation von Aufgaben und zur Nachverfolgung des Fortschritts. Erstellt mit JavaScript, HTML und CSS für eine saubere, benutzerfreundliche Oberfläche, mit Account-Integration für ein personalisiertes Erlebnis.",
+        "descriptiongerman": "Kanban-ähnliches Aufgabenverwaltungs- und Team-Kollaborationsboard mit Drag-and-Drop-Schnittstellen zur Organisation von Aufgaben und zur Nachverfolgung des Fortschritts. Erstellt mit JavaScript, HTML und CSS für eine saubere, benutzerfreundliche Oberfläche, mit Account-Integration für ein personalisiertes Erlebnis.",
 
         "description": "Developed a Kanban-style task management and team collaboration board with drag-and-drop interfaces for task organization and progress tracking. Built with JavaScript, HTML, and CSS for a clean, user-friendly interface, featuring account integration for a personalized experience.",
         "github": "https://github.com/michaelwoelfel/Join-Kanban-Board"
